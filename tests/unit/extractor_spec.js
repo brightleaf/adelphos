f1e44c03c32844084e92503771ef5af4974bf7e9
Kevin Isom <kevin.isom@gmail.com>
2015-03-18 23:27:26 +1300

    First test spec

---
+++ tests/unit/extractor_spec.js
@@ -0,0 +1,7 @@
+import assert from 'assert';
+
+describe('extractor module', () =>{
+    it('should return a flattened object given a google profile', function () {
+        assert.equal(1,1);
+    });
+});
