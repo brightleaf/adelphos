c6a82a5163e03cb6f9b8b3e64449fa0aafc3e62d
Kevin Isom <kevin.isom@gmail.com>
2015-03-19 22:21:09 +1300

    capture profile for testing

--- auth/extractor.js
+++ auth/extractor.js
@@ -1,14 +1,16 @@
 export default  (profile, callback) => {
     let { id, displayName, name, emails, photos, gender } = profile;
-
+    let email = emails[0].value;
+    let photo = photos[0].value;
+    let auth = 'google';
     callback(null, { 
         id, 
         displayName, 
         name, 
-        emails, 
-        photos, 
+        email, 
+        photo, 
         gender,
-        auth: 'google'
+        auth
     });
 
 }
